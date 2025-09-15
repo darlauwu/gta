6ced9e05e5a0a1d732ba69a511d1e95140f0ab60
darlauwu <piotrpolish19@gmail.com>
2025-09-15 18:36:38 -0500

    Delete save-link.php

--- save-link.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST["link"]) && !empty($_POST["link"])){
-  $link = trim($_POST["link"]);
-  file_put_contents("links.txt", $link.PHP_EOL, FILE_APPEND);
-}
-header("Location: drive-leaks.php");
-exit();
-?>
